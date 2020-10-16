cc8259ea869ee442d8f85de49f18748c9f6dfbaa
Jafeth Díaz <jafeth.diazc@gmail.com>
2020-10-16 18:09:18 -0500

    basic setup

---
+++ app/javascript/packs/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Home from "../components/Home"
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Home />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
