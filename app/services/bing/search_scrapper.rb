cd814c879a81e871926d4fa12b9cdbd2bf8efbf3
Jafeth Díaz <jafeth.diazc@gmail.com>
2020-10-18 14:00:50 -0500

    add basic scrappers

---
+++ app/services/bing/search_scrapper.rb
@@ -0,0 +1,38 @@
+module Bing
+  class SearchScrapper
+    BASE_URL = 'https://www.bing.com/search'.freeze
+    PAGE_SIZE = 10
+    DESKTOP_USER_AGENT =
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0'
+        .freeze
+
+    def initialize(term, page = 1)
+      @term = term
+      @page = page
+    end
+
+    def run
+      document = load_document
+      result_items = document.css('#b_results .b_algo')
+      result_items.map do |result|
+        header, body = result.children
+        link = header.css('a').first
+        description = body.css('p').first
+        OpenStruct.new(
+          url: link.attributes['href'].value,
+          title: link.text,
+          description: description.text
+        )
+      end
+    end
+
+    private
+
+    def load_document
+      offset = @page * PAGE_SIZE
+      url = "#{BASE_URL}?q=#{@term}&first=#{offset}"
+      html = HTTParty.get(url, headers: { 'User-Agent' => DESKTOP_USER_AGENT })
+      Nokogiri.HTML(html)
+    end
+  end
+end
